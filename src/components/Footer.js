4ee7624c50d91cc0070b21307a705a16f1c7bcb5
michael-a-tobias <michael.a.tobias@outlook.com>
2022-06-27 16:53:05 -0700

    End of day 1. Working on Frontend Structure Trello card.

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>It's a Tarantino picture over here!!</div>
+    )
+  }
+}
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+  render() {
+    return (
+      <div>This is the header, baby!!</div>
+    )
+  }
+}
+export default Header
